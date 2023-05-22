8172f306e6060ab64c11c02ac600b240568862b6
PalumboAndrea <andreaseiuno.ap@gmail.com>
2023-05-22 17:26:07 +0200

    Bonus but Milestone 3 is static

--- src/main/java/org/java/dbNations/Main.java
+++ src/main/java/org/java/dbNations/Main.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Main {
 	
@@ -15,16 +17,23 @@ public static void main(String[] args) throws Exception {
 		String user = "root";
 		String password = "code";
 		
-		try (Connection con = DriverManager.getConnection(url, user, password)){
+		try (Scanner sc = new Scanner(System.in);
+			Connection con = DriverManager.getConnection(url, user, password)){
     
 			String sql = " SELECT * FROM countries "
 					+ " JOIN regions "
 					+ " ON countries.region_id  = regions.region_id "
 					+ " JOIN continents "
 					+ " ON regions.continent_id = continents.continent_id "
+					+ " WHERE countries.name LIKE '%ita%'"
 					+ " ORDER BY countries.name ASC ";
 			
+//			System.out.println("Esegui una ricerca:");
+//			String userSearch = sc.nextLine();
+			
 			try (PreparedStatement ps = con.prepareStatement(sql)) {
+				
+//				ps.setString(1, userSearch);
 				
 				try (ResultSet rs = ps.executeQuery()) {
 					
@@ -38,15 +47,67 @@ public static void main(String[] args) throws Exception {
 						
 						System.out.println(name + " - " + id + " - " 
 								+ area + " - " + regionName + " - " + continentName);
+						
 					}
-				}				
+						System.out.println("Seleziona un ID tra quelli proposti");
+						int userSearchId = sc.nextInt();
+						sc.nextLine();
+					
+						sql = " SELECT DISTINCT languages.`language`, countries.name, countries.country_id, country_stats.population, country_stats.`year`, country_stats.gdp FROM countries "
+								+ " JOIN regions "
+								+ " ON countries.region_id  = regions.region_id "
+								+ " JOIN continents "
+								+ " ON regions.continent_id = continents.continent_id "
+								+ " JOIN country_languages "
+								+ " ON country_languages.country_id = countries.country_id "
+								+ " JOIN languages "
+								+ " ON languages.language_id = country_languages.language_id"
+								+ " JOIN country_stats "
+								+ " ON country_stats.country_id = countries.country_id"
+								+ " WHERE countries.country_id LIKE ? "
+								+ " ORDER BY country_stats.`year` ASC ";
+						
+						try (PreparedStatement ps1 = con.prepareStatement(sql)) {
+							
+							ps1.setLong(1, userSearchId);
+
+							try (ResultSet rs1 = ps1.executeQuery()) {
+								
+								HashSet<String> languages = new HashSet<String>();
+								
+								String language;
+								String name = null;
+								int population = 0;
+								int year = 0;
+								Long gdp = null;
+							
+								while(rs1.next()) {
+									
+									language = rs1.getString(1);
+									languages.add(language);
+									name = rs1.getString(2);
+									population = rs1.getInt(4);
+									year = rs1.getInt(5);
+									gdp = rs1.getLong(6);
+								}
+								
+								System.out.println("\nDetalis for country: " + name + "\n"
+										+ "Languages: " + languages + "\n" 
+										+ "Most recent stats:" + "\n" 
+										+ "Year: " + year + "\n"
+										+ "Population: " + population + "\n"
+										+ "GDP: " + gdp + "\n");
+								
+								}
+							}
+						}				
 			} catch (SQLException ex) {
 				System.err.println("Query not well formed");
 			}
 		
-		} catch (SQLException ex) {
+			} catch (SQLException ex) {
 			System.err.println("Error during connection to db");
-			}
+		}
 
 	}
 
